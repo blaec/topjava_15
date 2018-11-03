ec8c37111849bdedd57c7db860d5e97646c291d1
blaec <odeskonst@yandex.ru>
2018-11-03 17:23:29 +0200

    w02_70_03 destroy Spring Context and check Id in Meal controller

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import javax.servlet.ServletConfig;
@@ -16,19 +15,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Collection;
 import java.util.Objects;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
-
+    ConfigurableApplicationContext appCtx;
     private MealRestController controller;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
         controller = appCtx.getBean(MealRestController.class);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        appCtx.close();
     }
 
     @Override
@@ -43,9 +47,7 @@ public class MealServlet extends HttpServlet {
                 String dateTo = request.getParameter("dateTo");
                 String timeFrom = request.getParameter("timeFrom");
                 String timeTo = request.getParameter("timeTo");
-                Collection<Meal> filtered = controller.getFiltered(dateFrom, dateTo, timeFrom, timeTo);
-                request.setAttribute("meals",
-                        MealsUtil.getWithExceeded(filtered, MealsUtil.DEFAULT_CALORIES_PER_DAY));
+                request.setAttribute("meals", controller.getFiltered(dateFrom, dateTo, timeFrom, timeTo));
                 request.getRequestDispatcher("/meals.jsp").forward(request, response);
                 break;
             case "default":
@@ -60,7 +62,7 @@ public class MealServlet extends HttpServlet {
                 if (meal.isNew()) {
                     controller.create(meal);
                 } else {
-                    controller.update(meal);
+                    controller.update(meal, Integer.valueOf(id));
                 }
                 response.sendRedirect("meals");
         }
@@ -88,8 +90,7 @@ public class MealServlet extends HttpServlet {
             case "all":
             default:
                 log.info("getAll");
-                request.setAttribute("meals",
-                        MealsUtil.getWithExceeded(controller.getAll(), MealsUtil.DEFAULT_CALORIES_PER_DAY));
+                request.setAttribute("meals", controller.getAll());
                 request.getRequestDispatcher("/meals.jsp").forward(request, response);
                 break;
         }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 
 @Controller
 public class MealRestController {
@@ -25,7 +29,8 @@ public class MealRestController {
         return service.create(meal);
     }
 
-    public Meal update(Meal meal) {
+    public Meal update(Meal meal, int id) {
+        assureIdConsistent(meal, id);
         meal.setUserId(SecurityUtil.getAuthUserId());
         log.info("update meal with id {} for user {}", meal.getId(), meal.getUserId());
         return service.update(meal);
@@ -41,18 +46,22 @@ public class MealRestController {
         return service.get(id, SecurityUtil.getAuthUserId());
     }
 
-    public Collection<Meal> getAll() {
+    public Collection<MealWithExceed> getAll() {
         log.info("get all meals for user {}", SecurityUtil.getAuthUserId());
-        return service.getAll(SecurityUtil.getAuthUserId());
+        return convertToMealWithExceel(service.getAll(SecurityUtil.getAuthUserId()));
     }
 
-    public Collection<Meal> getFiltered(String dateFrom, String dateTo, String timeFrom, String timeTo) {
+    public Collection<MealWithExceed> getFiltered(String dateFrom, String dateTo, String timeFrom, String timeTo) {
         LocalDate fromDate = dateFrom.isEmpty() ? LocalDate.MIN : LocalDate.parse(dateFrom);
         LocalDate toDate = dateTo.isEmpty() ? LocalDate.MAX : LocalDate.parse(dateTo);
         LocalTime fromTime = timeFrom.isEmpty() ? LocalTime.MIN : LocalTime.parse(timeFrom);
         LocalTime toTime = timeTo.isEmpty() ? LocalTime.MAX : LocalTime.parse(timeTo);
         log.info("filtering meals from date {} to date {} and between times {} and {}", fromDate, toDate, fromTime, toTime);
 
-        return service.getFiltered(fromDate, toDate, fromTime, toTime, SecurityUtil.getAuthUserId());
+        return convertToMealWithExceel(service.getFiltered(fromDate, toDate, fromTime, toTime, SecurityUtil.getAuthUserId()));
+    }
+
+    private Collection<MealWithExceed> convertToMealWithExceel(Collection<Meal> meals) {
+        return MealsUtil.getWithExceeded(meals, MealsUtil.DEFAULT_CALORIES_PER_DAY);
     }
 }
