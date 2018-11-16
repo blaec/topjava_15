db970080c66af0ad3ee472ca5da53f6ba6ff2537
blaec <odeskonst@yandex.ru>
2018-11-16 15:04:02 +0200

    w04_01_00 Meal.class annotation

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,45 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+//        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM User u WHERE u.id=:id"),
+//        @NamedQuery(name = Meal.BY_EMAIL, query = "SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email=?1"),
+//        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT u FROM User u LEFT JOIN FETCH u.roles ORDER BY u.name, u.email"),
+})
+@Entity
+@Table(name = "meals",
+       uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"},
+                                              name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
+
+//    public static final String DELETE = "Meal.delete";
+//    public static final String BY_EMAIL = "Meal.getByEmail";
+//    public static final String ALL_SORTED = "Meal.getAllSorted";
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(min = 5, max = 255)
     private String description;
 
+    @Column(name = "calories")
+    @Range(min = 10, max = 10000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="user_id")
     private User user;
 
     public Meal() {
